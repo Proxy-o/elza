c3666ae983bb05ab8c8f039793fe8cf50b425008
tianazmah <64625379+tianazmah@users.noreply.github.com>
2023-05-30 16:14:27 +0100

    [ui] created Subhead

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
