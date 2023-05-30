655d3c4ccf3bbf595b9c72dff109434328e0e24c
OPERLY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-05-30 16:15:55 +0100

    [ui] adding carousel to home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
 import React from "react";
+import Image from "next/image";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const Page = () => {
-  return <div>Index from HOme</div>;
+  return (
+    <div className="bg-green-600">
+      <Splide aria-label="My Favorite Images">
+        <SplideSlide>
+          <div className="w-full md: bg-red-400">
+            <Image src="/images/carousel/img1.webp" alt="Image 1" fill={true} />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <Image src="/images/carousel/img2.webp" alt="Image 2" fill={true} />
+        </SplideSlide>
+        <SplideSlide>
+          <Image src="/images/carousel/img3.webp" alt="Image 2" fill={true} />
+        </SplideSlide>
+      </Splide>
+    </div>
+  );
 };
 
 export default Page;
