f7a9bce6ba879bccd311c72d20333d5731343e70
OPERLY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-05-30 19:30:11 +0100

    [ui] add a card in carousel

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -9,20 +9,26 @@ const Page = () => {
   const options = {
     type: "loop",
     gap: "1rem",
-    autoplay: true,
+    autoplay: false,
     pauseOnHover: true,
     resetProgress: false,
     interval: 3000,
+    arrows: false,
   };
   return (
-    <div className="bg-black  h-[50rem]">
+    <div className=" ">
       <Splide
         options={options}
         aria-label="My Favorite Images"
         className="border border-black h-full"
       >
         <SplideSlide>
-          <div className="h-[50rem] w-screen   ">
+          <div className="h-[30rem] md:h-[40rem] w-screen  flex ">
+            <div className="bg-primary opacity-80 relative z-10 my-auto h-1/3 m-auto w-4/6 md:mx-0 md:w-4/6 flex">
+              <h1 className="text-white  font-bold  m-auto text-center text-lg md:text-2xl">
+                Une recherche constante de la satisfaction du client
+              </h1>
+            </div>
             <Image
               src="/images/carousel/img4.webp"
               alt="Image 1"
