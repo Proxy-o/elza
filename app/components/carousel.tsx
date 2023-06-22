c88853cd51b2b8dc5ab4841d54ff4f094f393794
PROXY <96418976+Othmane-ait-taleb@users.noreply.github.com>
2023-06-22 15:27:26 +0100

    [refactor] move carousel to a new comp

---
+++ app/components/carousel.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Image from "next/image";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+
+function Carousel() {
+  const options = {
+    type: "loop",
+    gap: "1rem",
+    autoplay: false,
+    pauseOnHover: true,
+    resetProgress: false,
+    interval: 3000,
+    arrows: false,
+  };
+  return (
+    <div>
+      <Splide
+        options={options}
+        aria-label="My Favorite Images"
+        className="border border-black h-full"
+      >
+        <SplideSlide>
+          <div className="h-[30rem] md:h-[40rem] w-screen  flex justify-center items-center">
+            <div className="relative z-10 text-white flex justify-center flex-col ">
+              <h1 className="   m-auto text-center text-2xl md:text-5xl font-extrabold">
+                Une recherche constante de la satisfaction du client
+              </h1>
+              <button className="bg-none text-center mt-20 border rounded-lg w-1/3 h-10 mx-auto">
+                En savoir plus
+              </button>
+            </div>
+            <Image
+              src="/images/carousel/img4.webp"
+              alt="Image 1"
+              fill
+              className="rounded-md object-cover  brightness-50"
+            />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className="h-[30rem] md:h-[40rem] w-screen  flex justify-center items-center">
+            <div className="relative z-10 text-white flex justify-center flex-col ">
+              <h1 className="   m-auto text-center text-2xl md:text-5xl font-extrabold">
+                Une recherche constante de la satisfaction du client
+              </h1>
+              <button className="bg-none text-center mt-20 border rounded-lg w-1/3 h-10 mx-auto">
+                En savoir plus
+              </button>
+            </div>
+            <Image
+              src="/images/carousel/img2.webp"
+              alt="Image 2"
+              fill={true}
+              className="rounded-md object-cover brightness-50"
+            />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className="h-[30rem] md:h-[40rem] w-screen  flex justify-center items-center">
+            <div className="relative z-10 text-white flex justify-center flex-col ">
+              <h1 className="   m-auto text-center text-2xl md:text-5xl font-extrabold">
+                Une recherche constante de la satisfaction du client
+              </h1>
+              <button className="bg-none text-center mt-20 border rounded-lg w-1/3 h-10 mx-auto">
+                En savoir plus
+              </button>
+            </div>
+            <Image
+              src="/images/carousel/img3.webp"
+              alt="Image 2"
+              fill={true}
+              className="rounded-md object-cover brightness-50"
+            />
+          </div>
+        </SplideSlide>
+      </Splide>
+    </div>
+  );
+}
+
+export default Carousel;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,22 +1,12 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import { GiFactory } from "react-icons/gi";
 import Card from "../components/card/card";
+import Carousel from "../components/carousel";
 
 const Page = () => {
-  const options = {
-    type: "loop",
-    gap: "1rem",
-    autoplay: false,
-    pauseOnHover: true,
-    resetProgress: false,
-    interval: 3000,
-    arrows: false,
-  };
   const cards = [
     {
       icon: <GiFactory size={100} />,
@@ -37,66 +27,9 @@ const Page = () => {
   ];
   return (
     <div className=" ">
-      <Splide
-        options={options}
-        aria-label="My Favorite Images"
-        className="border border-black h-full"
-      >
-        <SplideSlide>
-          <div className="h-[30rem] md:h-[40rem] w-screen  flex justify-center items-center">
-            <div className="relative z-10 text-white flex justify-center flex-col ">
-              <h1 className="   m-auto text-center text-2xl md:text-5xl font-extrabold">
-                Une recherche constante de la satisfaction du client
-              </h1>
-              <button className="bg-none text-center mt-20 border rounded-lg w-1/3 h-10 mx-auto">
-                En savoir plus
-              </button>
-            </div>
-            <Image
-              src="/images/carousel/img4.webp"
-              alt="Image 1"
-              fill
-              className="rounded-md object-cover  brightness-50"
-            />
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="h-[30rem] md:h-[40rem] w-screen  flex justify-center items-center">
-            <div className="relative z-10 text-white flex justify-center flex-col ">
-              <h1 className="   m-auto text-center text-2xl md:text-5xl font-extrabold">
-                Une recherche constante de la satisfaction du client
-              </h1>
-              <button className="bg-none text-center mt-20 border rounded-lg w-1/3 h-10 mx-auto">
-                En savoir plus
-              </button>
-            </div>
-            <Image
-              src="/images/carousel/img2.webp"
-              alt="Image 2"
-              fill={true}
-              className="rounded-md object-cover brightness-50"
-            />
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className="h-[30rem] md:h-[40rem] w-screen  flex justify-center items-center">
-            <div className="relative z-10 text-white flex justify-center flex-col ">
-              <h1 className="   m-auto text-center text-2xl md:text-5xl font-extrabold">
-                Une recherche constante de la satisfaction du client
-              </h1>
-              <button className="bg-none text-center mt-20 border rounded-lg w-1/3 h-10 mx-auto">
-                En savoir plus
-              </button>
-            </div>
-            <Image
-              src="/images/carousel/img3.webp"
-              alt="Image 2"
-              fill={true}
-              className="rounded-md object-cover brightness-50"
-            />
-          </div>
-        </SplideSlide>
-      </Splide>
+      <div>
+        <Carousel />
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3">
         {cards.map((card, index) => {
