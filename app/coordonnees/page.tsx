533755944c8bb6b3edadc83749297e64f563bb9b
Tian <hamza2nait@gmail.com>
2023-06-25 13:59:09 +0100

    [WIP] creating coordonnees page

--- app/coordonnees/page.tsx
+++ app/coordonnees/page.tsx
@@ -2,32 +2,38 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div>
-      <div className="grid grid-cols-1 pt-28 pb-20">
-        <div className="flex flex-col justify-start items-start px-16">
-          <h1 className="text-4xl font-bold text-slate-800 ">Coordonnées</h1>
-            <p className="text-slate-800 text-lg pl-9">
-                Adresse: 26, Route Bouskoura  Bd Med V, 3ème étage  26100 Berrechid.
-            </p>
-            <div className="flex-col lg:flex w-full">
-                <p className="text-slate-800 text-lg pl-9">
-                    Service Administratif:  Mr Brahim EL ABBOUBI
-                </p>
-                <span className="text-blue-500 ml-auto mr-[40%]">+212 661 38 81 95</span>
-            </div>
-            <div className="flex-col lg:flex w-full">
-                <p className="text-slate-800 text-lg pl-9">
-                Service technique Mr Rachid ZAHIR e-mail
-                </p>
-               <span className="text-blue-500 ml-auto mr-[40%]">+212 661 50 95 53</span>
-            </div>
-            <div className="flex-col lg:flex w-full">
-                <p className="text-slate-800 text-lg pl-9">
+    <div className="h-screen">
+      <div className="grid grid-cols-1 pt-28 pb-20 mx-4 lg:mx-16">
+        <div className="flex flex-col justify-center items-center px-16 pb-10">
+          <h1 className="text-4xl font-bold text-primary">Coordonnées</h1>
+        </div>
+        <p className="flex flex-col lg:flex-row text-slate-800 text-lg pl-9 mb-8 items-center">
+            <span className="text-2xl font-bold mr-2">Adresse: </span>26, Route Bouskoura  Bd Med V, 3ème étage  26100 Berrechid.
+        </p>
+        <div className="flex flex-col lg:flex-row w-full">
+          <div className="flex text-slate-800 pl-9 text-xl font-bold mr-2">
+              Service Administratif:
+          </div>
+          <div className="m-auto lg:ml-auto">
+            Mr.Brahim EL ABBOUBI
+          </div>
+          <span className="text-blue-500 mx-auto lg:ml-3 lg:mr-[40%]">+212 661 38 81 95</span>
+        </div>
+        <div className="flex flex-col lg:flex-row w-full">
+          <div className="flex text-slate-800 pl-9 text-xl font-bold mr-2">
+            Service technique: 
+          </div>
+          <div className="m-auto lg:ml-auto">
+            Mr.Rachid ZAHIR
+          </div>
+          <span className="text-blue-500 mx-auto lg:ml-3 lg:mr-[40%]">+212 661 50 95 53</span>
+        </div>
+            <div className="flex flex-col lg:flex-row w-full">
+                <p className="text-slate-800 pl-9 text-xl font-bold mr-2">
                     Fax: 
                 </p>
-                <span className="text-blue-500 ml-auto mr-[40%]">+212 522 32 21 91</span>
+                <span className="text-blue-500 mx-auto lg:ml-auto lg:mr-[40%]">+212 522 32 21 91</span>
             </div>
-        </div>
       </div>
     </div>
   );
